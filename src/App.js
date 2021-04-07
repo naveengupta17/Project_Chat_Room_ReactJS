3b01e3dcf912be7cdb3583aa555008db47efde30
naveengupta17 <naveen.gupta_cs18@gla.ac.in>
2021-04-07 09:26:00 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import {ChatEngine} from 'react-chat-engine';
+import './App.css';
+function App() {
+  return (
+    <ChatEngine
+      height="100vh"
+      projectID="1953f7f9-427e-4c24-add3-e4a57b80d517"
+      userName="naveen171071"
+      userSerect="123123"
+    />
+  );
+}
+
+export default App;
